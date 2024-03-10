c46fc11eeae6d9677319ce817e06fc68239f468c
nekofood <sonicroblox@gmail.com>
2024-03-10 14:18:57 +0800

    Reduce cos and sin calls

--- src/Particle.java
+++ src/Particle.java
@@ -1,5 +1,7 @@
 public class Particle {
-    private double x, y; 
+    private double x, y, vx, vy;
+	//x, y = world coordinates
+	//vx, vy = movement vector
     private double velocity; 
     private double angle; 
 
@@ -7,45 +9,55 @@ public class Particle {
         this.x = x;
         this.y = y;
         this.velocity = velocity;
+		this.vx = velocity * Math.cos(Math.toRadians(angle));
+		this.vy = velocity * Math.sin(Math.toRadians(angle));
         this.angle = angle;
     }
 
     public void move(double deltaTime) {
-        x += velocity * Math.cos(Math.toRadians(angle)) * deltaTime;
-        y += velocity * Math.sin(Math.toRadians(angle)) * deltaTime;
+        x += vx * deltaTime; 
+        y += vy * deltaTime;
     }
 
     public double get_next_x(double deltaTime) {
-        return x + velocity * Math.cos(Math.toRadians(angle)) * deltaTime;
+        return x + vx * deltaTime;
     }
 
     public double get_next_y(double deltaTime) {
-        return y + velocity * Math.sin(Math.toRadians(angle)) * deltaTime;
+        return y + vy * deltaTime;
     }
 
     public void bounceHorizontal() {
         angle = 180 - angle;
+		recalculateV();
     }
     
     public void setAngle(double angle){
         this.angle = angle;
+		recalculateV();
     }
     public void bounceVertical() {
         angle = -angle;
+		recalculateV();
     }
 
     public void bounceOffWall(double wallAngle) {
         double wallNormalX = Math.cos(wallAngle + Math.PI / 2); 
         double wallNormalY = Math.sin(wallAngle + Math.PI / 2);
     
-        double dotProduct = getVelocityX() * wallNormalX + getVelocityY() * wallNormalY;
+        double dotProduct = vx * wallNormalX + vy * wallNormalY;
     
-        double reflectX = getVelocityX() - 2 * dotProduct * wallNormalX;
-        double reflectY = getVelocityY() - 2 * dotProduct * wallNormalY;
+        double reflectX = vx - 2 * dotProduct * wallNormalX;
+        double reflectY = vy - 2 * dotProduct * wallNormalY;
     
         angle = Math.toDegrees(Math.atan2(reflectY, reflectX));
+		recalculateV();
     }
     
+	public void recalculateV() {
+		vx = velocity * Math.cos(Math.toRadians(angle));
+		vy = velocity * Math.sin(Math.toRadians(angle));
+	}
 
     public double getAngle() {
         return angle;
@@ -54,6 +66,6 @@ public class Particle {
     public double getX() { return x; }
     public double getY() { return y; }
     public double getVelocity() { return velocity; }
-    public double getVelocityX() { return velocity * Math.cos(Math.toRadians(angle)); }
-    public double getVelocityY() { return velocity * Math.sin(Math.toRadians(angle)); }
+    public double getVelocityX() { return vx; }
+    public double getVelocityY() { return vy; }
 }
